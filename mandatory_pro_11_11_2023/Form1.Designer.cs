9b2d11e02a76f304e415ee492dc5443cc8e707e0
Vassilis <114075186+vasloui@users.noreply.github.com>
2023-11-13 12:32:29 +0200

    Added the minimum functionality.

---
+++ mandatory_pro_11_11_2023/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace mandatory_pro_11_11_2023
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.shapeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.crossToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.diamondToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.isoscelesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kiteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.parallelogramToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rectangleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rightAngleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.starToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.trapeziumToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.octagonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eraserToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.shapeToolStripMenuItem,
+            this.eraserToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(997, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // shapeToolStripMenuItem
+            // 
+            this.shapeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.crossToolStripMenuItem,
+            this.diamondToolStripMenuItem,
+            this.isoscelesToolStripMenuItem,
+            this.kiteToolStripMenuItem,
+            this.parallelogramToolStripMenuItem,
+            this.rectangleToolStripMenuItem,
+            this.rightAngleToolStripMenuItem,
+            this.starToolStripMenuItem,
+            this.trapeziumToolStripMenuItem,
+            this.octagonToolStripMenuItem});
+            this.shapeToolStripMenuItem.Name = "shapeToolStripMenuItem";
+            this.shapeToolStripMenuItem.Size = new System.Drawing.Size(64, 24);
+            this.shapeToolStripMenuItem.Text = "Shape";
+            this.shapeToolStripMenuItem.Click += new System.EventHandler(this.shapeToolStripMenuItem_Click);
+            // 
+            // crossToolStripMenuItem
+            // 
+            this.crossToolStripMenuItem.Name = "crossToolStripMenuItem";
+            this.crossToolStripMenuItem.Size = new System.Drawing.Size(184, 26);
+            this.crossToolStripMenuItem.Text = "Cross";
+            this.crossToolStripMenuItem.Click += new System.EventHandler(this.crossToolStripMenuItem_Click);
+            // 
+            // diamondToolStripMenuItem
+            // 
+            this.diamondToolStripMenuItem.Name = "diamondToolStripMenuItem";
+            this.diamondToolStripMenuItem.Size = new System.Drawing.Size(184, 26);
+            this.diamondToolStripMenuItem.Text = "Diamond";
+            this.diamondToolStripMenuItem.Click += new System.EventHandler(this.diamondToolStripMenuItem_Click);
+            // 
+            // isoscelesToolStripMenuItem
+            // 
+            this.isoscelesToolStripMenuItem.Name = "isoscelesToolStripMenuItem";
+            this.isoscelesToolStripMenuItem.Size = new System.Drawing.Size(184, 26);
+            this.isoscelesToolStripMenuItem.Text = "Isosceles";
+            this.isoscelesToolStripMenuItem.Click += new System.EventHandler(this.isoscelesToolStripMenuItem_Click);
+            // 
+            // kiteToolStripMenuItem
+            // 
+            this.kiteToolStripMenuItem.Name = "kiteToolStripMenuItem";
+            this.kiteToolStripMenuItem.Size = new System.Drawing.Size(184, 26);
+            this.kiteToolStripMenuItem.Text = "Kite";
+            this.kiteToolStripMenuItem.Click += new System.EventHandler(this.kiteToolStripMenuItem_Click);
+            // 
+            // parallelogramToolStripMenuItem
+            // 
+            this.parallelogramToolStripMenuItem.Name = "parallelogramToolStripMenuItem";
+            this.parallelogramToolStripMenuItem.Size = new System.Drawing.Size(184, 26);
+            this.parallelogramToolStripMenuItem.Text = "Parallelogram";
+            this.parallelogramToolStripMenuItem.Click += new System.EventHandler(this.parallelogramToolStripMenuItem_Click);
+            // 
+            // rectangleToolStripMenuItem
+            // 
+            this.rectangleToolStripMenuItem.Name = "rectangleToolStripMenuItem";
+            this.rectangleToolStripMenuItem.Size = new System.Drawing.Size(184, 26);
+            this.rectangleToolStripMenuItem.Text = "Rectangle";
+            this.rectangleToolStripMenuItem.Click += new System.EventHandler(this.rectangleToolStripMenuItem_Click);
+            // 
+            // rightAngleToolStripMenuItem
+            // 
+            this.rightAngleToolStripMenuItem.Name = "rightAngleToolStripMenuItem";
+            this.rightAngleToolStripMenuItem.Size = new System.Drawing.Size(184, 26);
+            this.rightAngleToolStripMenuItem.Text = "Right Angle";
+            this.rightAngleToolStripMenuItem.Click += new System.EventHandler(this.rightAngleToolStripMenuItem_Click);
+            // 
+            // starToolStripMenuItem
+            // 
+            this.starToolStripMenuItem.Name = "starToolStripMenuItem";
+            this.starToolStripMenuItem.Size = new System.Drawing.Size(184, 26);
+            this.starToolStripMenuItem.Text = "Star";
+            this.starToolStripMenuItem.Click += new System.EventHandler(this.starToolStripMenuItem_Click);
+            // 
+            // trapeziumToolStripMenuItem
+            // 
+            this.trapeziumToolStripMenuItem.Name = "trapeziumToolStripMenuItem";
+            this.trapeziumToolStripMenuItem.Size = new System.Drawing.Size(184, 26);
+            this.trapeziumToolStripMenuItem.Text = "Trapezium";
+            this.trapeziumToolStripMenuItem.Click += new System.EventHandler(this.trapeziumToolStripMenuItem_Click);
+            // 
+            // octagonToolStripMenuItem
+            // 
+            this.octagonToolStripMenuItem.Name = "octagonToolStripMenuItem";
+            this.octagonToolStripMenuItem.Size = new System.Drawing.Size(184, 26);
+            this.octagonToolStripMenuItem.Text = "Octagon";
+            this.octagonToolStripMenuItem.Click += new System.EventHandler(this.octagonToolStripMenuItem_Click);
+            // 
+            // eraserToolStripMenuItem
+            // 
+            this.eraserToolStripMenuItem.Name = "eraserToolStripMenuItem";
+            this.eraserToolStripMenuItem.Size = new System.Drawing.Size(63, 24);
+            this.eraserToolStripMenuItem.Text = "Eraser";
+            this.eraserToolStripMenuItem.Click += new System.EventHandler(this.eraserToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button1.Location = new System.Drawing.Point(667, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 103);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(973, 397);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(406, 54);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(78, 31);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Text";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(503, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(88, 22);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(997, 512);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem shapeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem crossToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem diamondToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem isoscelesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kiteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem parallelogramToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rectangleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rightAngleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem starToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem trapeziumToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem octagonToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eraserToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ mandatory_pro_11_11_2023/Form1.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text.Json;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mandatory_pro_11_11_2023
+{
+    public partial class Form1 : Form
+    {
+        Pen pen, eraser;
+        Graphics graphics;
+        Bitmap bitmap;
+        int shape = 0;
+        String text = "";
+        bool draw;
+        bool erase;
+        PointF eLocation;
+
+
+        #region Initializers
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pen = new Pen(Color.Black, 3);
+            eraser = new Pen(pictureBox1.BackColor, 5);
+            bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            graphics = Graphics.FromImage(bitmap);
+            graphics.Clear(Color.White);
+            pictureBox1.Image = bitmap;
+            draw = false;
+            erase = false;
+        }
+        #endregion
+
+        #region Menu
+        private void shapeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            draw=true;
+            erase = false;
+        }
+
+        private void eraserToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            draw = false;
+            //erase = !erase;
+        }
+
+        #endregion
+
+        #region Menu Items
+        private void crossToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            shape = 1;
+            chooseColor();
+
+
+        }
+
+        private void diamondToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            shape = 2;
+            chooseColor();
+        }
+
+        private void isoscelesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            shape = 3;
+            chooseColor();
+        }
+
+        private void kiteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            shape = 4;
+            chooseColor();
+        }
+
+        private void parallelogramToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            shape = 5;
+            chooseColor();
+        }
+
+        private void rectangleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            shape = 6;
+            chooseColor();
+        }
+
+        private void rightAngleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            shape = 7;
+            chooseColor();
+        }
+
+        private void starToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            shape = 8;
+            chooseColor();
+        }
+
+        private void trapeziumToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            shape = 9;
+            chooseColor();
+        }
+
+        private void octagonToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            shape = 10;
+            chooseColor();
+        }
+        #endregion
+
+        #region Picture Box Mouse Events
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            erase = false;
+            PointF clicked = new PointF(e.X, e.Y);
+            if (draw && shape != 11)
+            {
+                List<PointF> points = placeShape(loadPoints(select_shape(shape)), clicked);
+                //PointF center = getCenter(points);
+                graphics.DrawLines(pen, points.ToArray());
+                //for (int i = 0; i < points.Count - 1; i++)
+                //{
+                //    graphics.DrawLine(pen, points[i], points[i + 1]);
+                //}
+
+            }
+            else if (draw && shape == 11)
+            {
+                //Prints text
+
+                graphics.DrawString(text, Font, pen.Brush, clicked);
+            }
+            pictureBox1.Refresh();
+            timer1.Enabled = false;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            erase = !draw;
+            
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (erase )
+            {
+                //timer1.Enabled = true;
+                PointF eLocation = e.Location;
+                graphics.DrawEllipse(eraser, eLocation.X, eLocation.Y, 10, 10);
+                pictureBox1.Refresh();
+
+            }
+        }
+
+        #endregion
+
+        #region Picture Box Timers
+
+        // Eraser Timer
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
+        }
+
+        #endregion
+
+        #region Buttons
+        //Clear Button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            clearAll();
+            draw = false;
+            erase = false;
+            pictureBox1.Refresh();
+        }
+        
+        //Text Button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            draw = true;
+            erase = false;
+            shape = 11;
+            pen.Brush = chooseColor(pen.Brush);
+        }
+
+        #endregion
+
+        #region Text Fields
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            text = textBox1.Text;
+        }
+        #endregion
+
+        #region Helper Functions
+        private void chooseColor()
+        {
+            if (!(colorDialog1.ShowDialog() == DialogResult.Cancel))
+            {
+                pen.Color = colorDialog1.Color;
+
+            }
+        }
+
+        private Brush chooseColor(Brush tool)
+        {
+            if (!(colorDialog1.ShowDialog() == DialogResult.Cancel))
+            {
+                tool = new SolidBrush(colorDialog1.Color);
+                
+                //var color = new Color();
+                //color = colorDialog1.Color;
+                //brush = color;
+                //Brush brush = new SolidBrush(colorDialog1.Color);
+            }
+            return tool;
+        }
+
+        //private void earse(PointF start, PointF stop)
+        //{
+        //    float width = Math.Abs(stop.X - start.X);
+        //    float height = Math.Abs(stop.Y - start.Y);
+        //    RectangleF rect = new RectangleF(start.X, start.Y, width, height);  
+        //    graphics.DrawEllipse(eraser, rect);
+        //}
+
+        private void clearAll()
+        {
+            graphics.Clear(pictureBox1.BackColor);
+        }
+
+        private String select_shape(int shape)
+        {
+            String json_Name = "";
+            switch (shape)
+            {
+                case 1:
+                    json_Name = "cross";
+                    break;
+                case 2:
+                    json_Name = "diamond";
+                    break;
+                case 3:
+                    json_Name = "isosceles";
+                    break;
+                case 4:
+                    json_Name = "kite";
+                    break;
+                case 5:
+                    json_Name = "parallelogram";
+                    break;
+                case 6:
+                    json_Name = "rectangle";
+                    break;
+                case 7:
+                    json_Name = "right_angle";
+                    break;
+                case 8:
+                    json_Name = "star";
+                    break;
+                case 9:
+                    json_Name = "trapezium";
+                    break;
+                case 10:
+                    json_Name = "octagon";
+                    break;
+                default:
+                    break;
+            }
+            return json_Name;
+        }
+
+        private List<PointF> loadPoints(String json_Name)
+        {
+            List<PointF> points = new List<PointF>();
+            String jsonString = File.ReadAllText(json_Name+".json");
+            points.Clear();
+            points = JsonSerializer.Deserialize<List<PointF>>(jsonString);
+            
+            return points;
+        }
+
+        private PointF getCenter(List<PointF> points)
+        {
+            float x = 0;
+            float y = 0;
+            foreach (PointF point in points)
+            {
+                x += point.X;
+                y += point.Y;
+            }
+            x = x / points.Count;
+            y = y / points.Count;
+            return new PointF(x, y);
+        }
+
+        
+
+        private List<PointF> placeShape(List<PointF> points, PointF center)
+        {
+            List<PointF> temp = new List<PointF>();
+            foreach (PointF point in points)
+            {
+                temp.Add(new PointF(point.X - center.X, point.Y - center.Y));
+            }
+            points.Clear();
+            points = temp;
+
+            return points;
+        }
+
+        #endregion
+    }
+}
